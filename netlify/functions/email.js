78d7293735bce62db969343a87a0fc7db1ace995
Łukasz <lsliwaradioluz@gmail.com>
2021-08-01 23:53:29 +0200

    CORS test

--- netlify/functions/email.js
+++ netlify/functions/email.js
@@ -1,34 +1,43 @@
 const nodemailer = require("nodemailer");
 
 const headers = {
+  'Access-Control-Allow-Credentials': true,
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
 };
 
-exports.handler = async function(event) {
-  const { from, to, subject, html } = JSON.parse(event.body)
-  const transporter = nodemailer.createTransport({
-    pool: true,
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+exports.handler = async function(event, context) {
+  console.log(event.httpMethod)
+  if (event.httpMethod === 'POST') {
+    const { from, to, subject, html } = JSON.parse(event.body)
+    const transporter = nodemailer.createTransport({
+      pool: true,
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    })
+  
+    const info = await transporter.sendMail({
+      from,
+      to,
+      subject,
+      html
+    });
+
+    return {
+      headers,
+      statusCode: 200,
+      body: JSON.stringify(info)
     }
-  })
-
-  const info = await transporter.sendMail({
-    from,
-    to,
-    subject,
-    html
-  });
-
-  return {
-    statusCode: 200,
-    headers,
-    body: JSON.stringify(info)
+  } else {
+    return {
+      headers,
+      statusCode: 200
+    }
   }
 }
