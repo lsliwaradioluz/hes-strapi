88188dccda3520bb073bb962e0712f29ba117394
Łukasz <lsliwaradioluz@gmail.com>
2021-08-01 23:20:42 +0200

    Create netlify lambda function

--- netlify/functions/email.js
+++ netlify/functions/email.js
@@ -1,6 +1,33 @@
-exports.handler = async function(event, context) {
+const nodemailer = require("nodemailer");
+
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+};
+
+exports.handler = async function(event) {
+  const { from, to, subject, html } = JSON.parse(event.body)
+  const transporter = nodemailer.createTransport({
+    pool: true,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  })
+
+  const info = await transporter.sendMail({
+    from,
+    to,
+    subject,
+    html
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify({message: "Hello World"})
-  };
+    body: JSON.stringify(info)
+  }
 }
