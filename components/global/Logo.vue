22fcbf2005b6fbbc021dd09c39e650253d31e367
Łukasz Śliwa <lsliwaradioluz@gmail.com>
2022-05-20 17:00:39 +0200

    feat: add new logo

---
+++ components/global/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <nuxt-link
+    class="logo"
+    to="/"
+    @click.native="$emit('clicked')"
+  >
+    <img
+      height="74"
+      width="120"
+      src="~/assets/images/logo.png"
+      alt="hes_logo"
+    >
+  </nuxt-link>
+</template>
+
+<style lang="scss" scoped>
+  .logo {
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+  }
+</style>
