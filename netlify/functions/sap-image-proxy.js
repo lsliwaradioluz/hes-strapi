f4a40dfeea196820351ca99fe43ca25a1a5207a7
Łukasz Śliwa <lsliwaradioluz@gmail.com>
2022-08-10 21:32:51 +0200

    feat: sap image proxy

---
+++ netlify/functions/sap-image-proxy.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+
+const headers = {
+  'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+};
+
+exports.handler = async function(event, context) {
+  const requestedPath = event.path;
+  const sapBaseUrl = 'https://api.c1jvi8hu9a-vsfspzooa1-d1-public.model-t.cc.commerce.ondemand.com/medias/';
+  const extractedSapContext = requestedPath.split("/").pop();
+  const finalUrl = `${sapBaseUrl}?context=${extractedSapContext}`;
+
+  return {
+    headers: {
+      "Location": finalUrl
+    },
+    statusCode: 302
+  }
+}
