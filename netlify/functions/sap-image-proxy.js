5db4841287462c12cb346251d2a0ff17e57c894b
Łukasz Śliwa <lsliwaradioluz@gmail.com>
2022-08-25 17:48:26 +0200

    chore: image proxy send request

--- netlify/functions/sap-image-proxy.js
+++ netlify/functions/sap-image-proxy.js
@@ -8,6 +8,8 @@ const headers = {
 };
 
 exports.handler = async function(event, context) {
+  axios.get('http://188.121.27.234:3000/sap-image-proxy/context/1234')
+
   const requestedPath = event.path;
   const sapBaseUrl = 'https://api.c1jvi8hu9a-vsfspzooa1-d1-public.model-t.cc.commerce.ondemand.com/medias/';
   const extractedSapContext = requestedPath.split("/").pop();
