d69673d0a153ee0e41669bccec8dccf3c680f602
Łukasz Śliwa <lsliwaradioluz@gmail.com>
2022-10-06 13:11:26 +0200

    chore: sap image proxy test

--- netlify/functions/sap-image-proxy.js
+++ netlify/functions/sap-image-proxy.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-
+//https://hesgym-fit.pl/.netlify/functions/sap-image-proxy
 const headers = {
   'Access-Control-Allow-Credentials': true,
   'Access-Control-Allow-Origin': '*',
@@ -8,13 +8,11 @@ const headers = {
 };
 
 exports.handler = async function(event, context) {
-  axios.get('http://188.121.27.234:3000/sap-image-proxy/context/1234')
-
   const requestedPath = event.path;
   const sapBaseUrl = 'https://api.c1jvi8hu9a-vsfspzooa1-d1-public.model-t.cc.commerce.ondemand.com/medias/';
   const extractedSapContext = requestedPath.split("/").pop();
   const finalUrl = `${sapBaseUrl}?context=${extractedSapContext}`;
-
+  console.log({ finalUrl });
   return {
     headers: {
       "Location": finalUrl
